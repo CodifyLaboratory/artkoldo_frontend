db6f8bcffed6a192c74d9c082585781017290aeb
aisuluuisakova <isakovaaisuluu.com>
2022-03-13 11:07:00 +0600

    h

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
-
-import PaintingsContainer from "./pages/PaintingsContainer";
-
-
+import MainPage from "./pages/MainPage/MainPage";
+import PaintingsPage from "./pages/PaintingsPage/PaintingsPage";
+import ProductPage from "./pages/ProductPage/ProductPage";
 
 function App() {
-
   return (
     <div className="App">
-        <PaintingsContainer products = { products } />
+      <MainPage />
     </div>
   );
 }
--- src/components/Products/ItemCards.jsx
+++ src/components/Products/ItemCards.jsx
@@ -1,21 +1,45 @@
-import React from "react";
-import { products } from "../../mockData/ListofProducts";
+import React, { useState, useEffect } from "react";
 import "./ItemCards.css";
-import { ProductItem } from "./ProductItem";
 import { Row, Col } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useNavigate } from "react-router-dom";
 
-export default function ItemCards({ items = [] }) {
+export default function ItemCards({ products = [] }) {
   // const list = items.length > 0 ? items : products;
 
+  let navigate = useNavigate();
+
+  function handleClick(id) {
+    navigate(`/paintings/${id}`);
+  }
 
   return (
-    <Row className="Item-Cards d-flex justify-content-between">
-      {products.map((product) => (
-        <Col sm={9} md={9} lg={6}>
-          <ProductItem product={product} key={product.id} />
-        </Col>
-      ))}
-    </Row>
+    <></>
+    // <Row className="Item-Cards d-flex justify-content-between">
+    //   {products.map((product) => (
+    //     <Col sm={6} md={6} lg={6} key={product.id}>
+    //       <div className="product-item">
+    //         <img
+    //           className="product-item_image"
+    //           src={product.image}
+    //           onClick={() => handleClick(product.id)}
+    //           alt=""
+    //         />
+    //         <div className="product-item_detailes">
+    //           <div className="product-item_detailes_col1">
+    //             <span className="product-item_title">{product.title}</span>
+    //             {/* <span className="product-item_aithor ">{product?.author}</span>
+    //             <span className="product-item_location">
+    //               {product?.location}
+    //             </span> */}
+    //           </div>
+    //           <div className="product-item_detailes_col2">
+    //             <span className="product-item_pice">{product.price}</span>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </Col>
+    //   ))}
+    // </Row>
   );
 }
