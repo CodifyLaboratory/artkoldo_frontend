191a440fe62d93b3ae94ac494f49be578b733f28
aisuluuisakova <isakovaaisuluu.com>
2022-03-15 21:15:05 +0600

    search-bar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,35 +2,55 @@ import React , {useState, useEffect} from 'react';
 import searchIcon from "../../images/search_icon.svg"
 import dropdownIcon from "../../images/arrow_down.svg"
 import { Form } from "react-bootstrap";
+import { API_URL } from "../../API/api";
+import axios from "axios";
 import './SearchBar.css'
 
 
 
 function SearchBar() {
-    // const [value, setValue]=useState('')
-    // const filteredProducts= product.filter(product=>{
-    //     return product.title.toLowerCase().includes(value.toLowerCase())
+    const [value, setValue]=useState('')
+
+    // const filteredProducts= products.filter(products=>{
+    //     return products.title.toLowerCase().includes(value.toLowerCase())
     // })
     // console.log(filteredProducts)
+
+    const [products, setProducts] = useState([]);
+    const [category, setCategory]  = useState('paintings');
+
+    useEffect(() => {
+      axios
+        .get(`${API_URL}/${category}`)
+        .then((response) => setProducts(response.data.results));
+        
+    }, [category]);
+
+    function getSelectedCategory(){
+      return setCategory(document.getElementById("categoryId").value)
+    }
+     
+    console.log("выбранная категория: ", category)
+
   return (
-    <form className="searchBar">
-        <Form.Select aria-label="Default select example" className="nb-search-select">
-         <option>Open this select menu</option>
-         <option value="1">Живопись</option>
-          <option value="2">Ремесленные изделия</option>
-          <option value="3">Керамика</option>
+    <div className="searchBar">
+        <Form.Select aria-label="Default select example" className="nb-search-select" id = "categoryId" onChange={getSelectedCategory}>
+         <option value ="" disabled selected>Поиск</option>
+         <option value="paintings">Живопись</option>
+          <option value="handicrafts">Ремесленные изделия</option>
+          <option value="ceramics">Керамика</option>
         </Form.Select>
-        <div className="search-fill">
-         <input className="search-bar" type="text" placeholder="Поиск" onChange={(event)=>console.log(event.target.value)} />
-        </div>
-        <div classname="search-right">
-            <div classname="search-sumbit">
-              <span classname="search-submit-img">
-                  <img src={searchIcon} className="search-icon"></img>
-              </span>
-             </div>
-        </div>
-    </form>
+         <input className="search-bar" type="text" onChange={(event)=>console.log(event.target.value)} />
+         {/* <div className="title">
+           <ul>
+             {
+               products.map(product=>(
+                 <li key = {product.title}></li>
+               ))
+             }
+           </ul>
+         </div> */}
+    </div>
   );
 }
 
