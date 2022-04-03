04f7d783b5305d24e59024cfbaf3f39bd49699e2
Gulzhan B <isoikigai@gmail.com>
2022-04-03 18:58:14 +0600

    aboutus

--- src/pages/AboutUs/AboutUsPage.jsx
+++ src/pages/AboutUs/AboutUsPage.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PageWrapper from "../../components/PageWrapper/index";
 import logo from "../../images/product-logo.jpeg";
-import { Link } from "react-router-dom";
+import { API_URL } from "../../API/api";
 import "./AboutUsPage.css";
+import axios from "axios";
 
 export default function AboutUsPage() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    axios
+      .get(`${API_URL}/api/info/about_us/`)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div>
       <PageWrapper>
@@ -20,37 +34,14 @@ export default function AboutUsPage() {
           <hr />
           <div className="short-info">
             <div className="short-info-text">
-              <span> Краткая информация о платформе</span>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Laudantium, tempperspiciatis non vitae quam, voluptates error?
-                Enim, velit. Commodi sint tempore temporibus tenetur? Nemo
-                quibusdam sapiente nam modi distinctio quasi nostrum corrupti
-                rem perferendis saepe. Quisquam eius dolor ea ducimus
-                <br />
-                <br />
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero
-                odit maxime hic ipsum tempore quae ratione id dolore. Ea
-                voluptates, culpa dolor, odio cum rerum omnis, sunt sint
-                excepturi tempora delectus iusto enim quasi vel assumenda quas
-                sequi? Harum accusamus fugiat voluptate quasi dicta iste dolorum
-                iure voluptates maiores. Sapiente doloremque possimus fugiat
-                voluptas nulla nemo nam vero distinctio pariatur, esse similique
-                debitis nostrum exercitationem aspernatur asperiores, dolores
-                soluta!
-                <br />
-                <br />
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad,
-                corporis molestias libero eveniet quaerat soluta laborum
-                explicabo maxime enim tempore odio dicta ducimus magnam ut
-                facilis culpa qui cupiditate pariatur dolore adipisci at,
-                repellat nobis. Consequatur libero dolorum reiciendis eaque ad
-                quos porro obcaecati corporis sequi quod.
-              </p>
+              <div className="short-info-title">
+                <span> Краткая информация о платформе</span>
+              </div>
+              <div className="short-info-description">
+                <p>{data?.description}</p>
+              </div>
             </div>
-            <div className="short-info-logo">
-              {/* <img src="logo" alt="../../images/logo.png"/> */}
-            </div>
+            <div className="short-info-logo"></div>
           </div>
         </div>
 
